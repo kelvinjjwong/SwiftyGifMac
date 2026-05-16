479e2c54d6389dd9ef79a828918fb88311fc3f3a
Kelvin Wong <kelvinjjwong@outlook.com>
2026-05-17 04:27:03 +0800

    debug

--- Package.swift
+++ Package.swift
@@ -14,11 +14,16 @@ let package = Package(
             name: "SwiftyGifMac",
             targets: ["SwiftyGifMac"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftyGifMac"),
+            dependencies: []),
         .testTarget(
             name: "SwiftyGifMacTests",
             dependencies: ["SwiftyGifMac"]
